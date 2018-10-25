b4b22625e7cd4879162b89f584b312721af8c6be
laurencejbelliott <laurencejbelliott@gmail.com>
2018-10-25 11:50:44 +0100

    Added movieInfo.py, example code from w5 workshop material adapted to use the Google Chrome/ Chromium driver. Requires chromedriver.exe in the root C directory of Windows.

---
+++ movieInfo.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import time
+
+br = webdriver.Chrome('C:/chromedriver.exe')
+br.implicitly_wait(15) # wait's for the page to get done loading before it does anything with it
+br.get('http://www.google.com/')
+# to fill out a form
+search = br.find_element_by_name('q')
+search.send_keys('blade runner')
+search.submit()
+time.sleep(5)
+print(br.title)
+
+
+driver = webdriver.Chrome()
