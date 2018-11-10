32b4385842aa12551a183901390a72032779e934
laurencejbelliott <laurencejbelliott@gmail.com>
2018-11-10 17:10:27 +0000

    movieSearch now also returns TMDb movieData as dictionary

--- movieSearch.py
+++ movieSearch.py
@@ -28,6 +28,16 @@ def movieSearch(text,year,date,API):
     else:
         API_KEY = '174496bdd4390eb9d2052615c460401b'
 
+        genresIDsURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=174496bdd4390eb9d2052615c460401b"
+        genresJSON = json.load(uOpen(genresIDsURL))['genres']
+
+        TMDbGenres = {}
+        for genre in genresJSON:
+            genreID = genre['id']
+            genreName = genre['name']
+
+            TMDbGenres[str(genreID)] = genreName
+
         APIurl = "https://api.themoviedb.org/3/search/movie?api_key=174496bdd4390eb9d2052615c460401b" \
                  "&query=" + text.replace(" ", "+")
         print APIurl
@@ -35,17 +45,36 @@ def movieSearch(text,year,date,API):
         movieData['results'] = []
 
         for result in resultJSON['results']:
-            movieData['Title'] = result['title']
-            movieData['Year'] = result['release_date'].split('-')[0]
-            movieData['']
+            resultData = {}
 
+            resultData['Title'] = result['title']
+            resultData['Year'] = result['release_date'].split('-')[0]
+            resultData['Date'] = result['release_date']
+            resultData['Reviews'] = result['vote_average']
+
+            if resultData['Reviews'] == 0:
+                resultData['Reviews'] = 'n/a'
+
+            resultData['Genre'] = result['genre_ids']
+
+            tempGenres = []
+            for genreID in resultData['Genre']:
+                tempGenres.append(TMDbGenres[str(genreID)])
+
+            tempGenresString = ', '.join(tempGenres)
+            resultData['Genre'] = tempGenresString
+
+            resultData['Plot'] = result['overview']
+
+            movieData['results'].append(resultData)
     return movieData
+
 
 if __name__ == "__main__":
     print "OMDB Results:\n"
     OMDBResults = movieSearch("John Wick","2014",None,"OMDB")
 
-
+    print ""
     for key in OMDBResults.keys():
         if str(type(OMDBResults[key])) == "<type 'unicode'>":
             print key + ": " + OMDBResults[key]
@@ -57,5 +86,11 @@ if __name__ == "__main__":
                 print ""
         print ""
 
-    # print "TMDb Results:\n"
-    # print movieSearch("John Wick","2014",None,"TMDb")
+    print "TMDb Results:\n"
+    TMDbResults = movieSearch("John Wick","2014",None,"TMDb")
+
+    print ""
+    for result in TMDbResults['results']:
+        for datumKey in result.keys():
+            print datumKey + ":", result[datumKey]
+        print ""
